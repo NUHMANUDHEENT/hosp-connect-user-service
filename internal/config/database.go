8b1527953559d202b3c309590de19209f8d032a3
nuhmanudheen t <nuhmanudheent@gmail.com>
2024-11-20 22:08:47 +0530

    logging add for env check

--- internal/config/database.go
+++ internal/config/database.go
@@ -16,9 +16,9 @@ func InitDatabase() *gorm.DB {
 		log.Fatal("DATABASE_URL environment variable not set")
 	}
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open("postgres://postgres:Nuhman%40456@postgres-db-service:5432/user_svc"), &gorm.Config{})
 	if err != nil {
-		log.Fatal("failed to connect with postgres......")
+		log.Fatal("failed to connect with postgres......",err)
 	}
 	err = db.AutoMigrate(&domain.Admin{}, &domain.Doctor{}, &domain.Patient{}, &domain.DoctorTokens{}, &domain.AvailabilitySlot{}, &domain.PatientPrescription{})
 	if err != nil {
